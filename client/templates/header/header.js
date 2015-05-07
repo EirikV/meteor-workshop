9ba8232d6ef0230f48b31e361d2f7a4481a5855e
EirikV <vigeland.eirik@gmail.com>
2015-05-07 14:46:04 +0200

    Updated header with text-area

---
+++ client/templates/header/header.js
@@ -0,0 +1,32 @@
+Template.header.helpers({
+	getName: function(profile) {
+
+		if(profile.firstname || profile.lastname) {
+		    return profile.firstname + ' ' + profile.lastname;
+		}
+
+		var firstname = [
+		    'Captain',
+		    'Jane',
+		    'John',
+		    'Ola',
+		    'Miss.',
+		    'Mr.',
+		    'Doctor',
+		    'Mrs.',
+		    'Sir.'
+		];
+
+		var lastname = [
+		    'NoName',
+		    'Nameless',
+		    'JohnDoe',
+		    'Nordmann',
+		    'HazNoName',
+		    'QuestionMark'
+		];
+
+
+		return firstname[Math.round(Math.random()*(firstname.length-1))] + ' ' + lastname[Math.round(Math.random()*(lastname.length-1))];
+	}
+});
