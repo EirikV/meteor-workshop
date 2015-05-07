18edb70b91d079af9ec1090ebf5f2e3512a6fbfe
kjohann <kris-90@live.no>
2015-05-07 19:14:13 +0200

    Refactor profile events: using submit

--- client/templates/profile/profile.js
+++ client/templates/profile/profile.js
@@ -1,40 +1,34 @@
 Template.editProfile.events({
-	"click .profileFormSubmit": function(e) {
+	'submit .profileForm': function(e) {
 		e.preventDefault();
-		var data = $("form.profileForm").serializeArray();
-		var profileInfo = _.object(_.pluck(data, "name"), _.pluck(data, "value"));
+		var data = $('form.profileForm').serializeArray();
+		var profileInfo = _.object(_.pluck(data, 'name'), _.pluck(data, 'value'));
 		
-		Meteor.call("updateUserProfile", profileInfo, function(error, result) {
+		Meteor.call('updateUserProfile', profileInfo, function(error, result) {
 			delete Session.keys['nameChange'];
 
 			if(error) {
 				Notifications.error(error.error);
 			} else {
-				Notifications.success("Successfully updated profile :)")
+				Notifications.success('Successfully updated profile :)')
 			}
 		});
 	},
-	"click .signout": function(e) {
-		e.preventDefault();
-		Meteor.logout();
-		Router.go("Posts");
-	},
-	"keyup input": function(e) {
+	'keyup input': function(e) {
 		var id = $(e.currentTarget).attr('name');
 
 		if(id === 'firstname' || id === 'lastname') {
 			var name = $('input[name="firstname"]').val() + ' ' + $('input[name="lastname"]').val();
-
 			Session.set('nameChange', name);
 		}
 	},
-	"change .profilePicUploadInput": function(event, template) {
+	'change .profilePicUploadInput': function(event, template) {
 		FS.Utility.eachFile(event, function(file) {
 			Images.insert(file, function (err, fileObj) {
 		  		if (err){
-					Notifications.error("Couldn't upload image :(");		 
+					Notifications.error('Error uploading image :(');		 
 			  	} else {
-					Meteor.call("updateProfileImage", fileObj._id);  
+					Meteor.call('updateProfileImage', fileObj._id);  
 			  	}
 			});
 		});
