fe74afbee79fae8e4b005283780e0eb69e3ea840
EirikV <vigeland.eirik@gmail.com>
2015-05-10 16:28:06 +0200

    Higlight of tags

--- client/templates/posts/newPost/newPost.js
+++ client/templates/posts/newPost/newPost.js
@@ -1,16 +1,13 @@
-var getFormValue = function(e) {
-    var data = $(e.currentTarget).serializeArray();
-    return _.pluck(data, "value")[0];
-};
-
 Template.newPost.events({
 	'submit form': function(e) {
 	    e.preventDefault();
 
-	    var text = getFormValue(e);
+	    var text = function() {
+           var data = $(e.currentTarget).serializeArray();
+           return _.pluck(data, "value")[0];
+       };
         
-        
-        Meteor.call("addPost", text, function(error, result) {
+        Meteor.call("addPost", text(), function(error, result) {
             if(!error) {
                 $(e.currentTarget).find('textarea').val('');
             } else {
