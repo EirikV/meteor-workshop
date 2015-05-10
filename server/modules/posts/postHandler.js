f18e587537a8fcaa5f19c2f24cf7522f7577eceb
EirikV <vigeland.eirik@gmail.com>
2015-05-10 16:50:26 +0200

    Removed stupid dobble closure

--- server/modules/posts/postHandler.js
+++ server/modules/posts/postHandler.js
@@ -1,45 +1,41 @@
-(function(){
+var PostHandler = function() {
 
-    var PostHandler = function() {
+    var filterTags = function(text) {
 
-        var filterTags = function(text) {
+        var tags = _.filter(text.split(' '), function(str){
+            return str.charAt(0) === '#';
+        });
 
-            var tags = _.filter(text.split(' '), function(str){
-                return str.charAt(0) === '#';
-            });
-
-            return _.map(tags, function(str) {
-                return str.replace(/[^A-Za-z0-9æøåÆØÅ]*/gi, '');
-            });
-        };
-
-        var instagramFromTags = function(tag) {
-            return Tweeteor.instagram.getImage(tag);
-        };
-
-        var addNew = function(text) {
-             if(!text || !text.trim()){
-                 throw new Meteor.Error('Empty post');
-             }
-            var tags = filterTags(text);
-            var id = Posts.insert({
-                text: text,
-                createdAt: new Date(),
-                userId: Meteor.userId(),
-                tags: tags
-            });
-            _.each(tags, function(tag) {
-                var tag = instagramFromTags(tag);
-                tag.postId = id;
-                Tags.insert(tag);
-            });      
-         };
-        return {
-            add: addNew
-        };
-
+        return _.map(tags, function(str) {
+            return str.replace(/[^A-Za-z0-9æøåÆØÅ]*/gi, '');
+        });
     };
 
-    Tweeteor.postHandler = new PostHandler();
+    var instagramFromTags = function(tag) {
+        return Tweeteor.instagram.getImage(tag);
+    };
 
-})();
+    var addNew = function(text) {
+         if(!text || !text.trim()){
+             throw new Meteor.Error('Empty post');
+         }
+        var tags = filterTags(text);
+        var id = Posts.insert({
+            text: text,
+            createdAt: new Date(),
+            userId: Meteor.userId(),
+            tags: tags
+        });
+        _.each(tags, function(tag) {
+            var tag = instagramFromTags(tag);
+            tag.postId = id;
+            Tags.insert(tag);
+        });
+     };
+    return {
+        add: addNew
+    };
+
+};
+
+Tweeteor.postHandler = new PostHandler();
