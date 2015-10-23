c0d835dd67a52fcf9567bcf85b3a476b7dd236b5
kjohann <kris-90@live.no>
2015-10-23 14:21:00 +0200

    Using method and added User collection
    Removed autopublish - using pub/sub instead
    Removed insecure - using methods instead

--- chat-app/chat-app.js
+++ chat-app/chat-app.js
@@ -1,6 +1,38 @@
 Messages = new Mongo.Collection('Messages');
+Users = new Mongo.Collection('Users');
+
+var createUser = function() { 
+    var firstname = [
+        'Captain',
+        'Jane',
+        'John',
+        'Ola',
+        'Miss.',
+        'Mr.',
+        'Doctor',
+        'Mrs.',
+        'Sir.'
+    ];
+
+    var lastname = [
+        'NoName',
+        'Nameless',
+        'JohnDoe',
+        'Nordmann',
+        'HazNoName',
+        'QuestionMark'
+    ];        
+
+    return {
+        firstname: firstname[Math.round(Math.random()*(firstname.length-1))],
+        lastname: lastname[Math.round(Math.random()*(lastname.length-1))] 
+    }
+};
 
 if (Meteor.isClient) {
+    Meteor.subscribe('messages');
+    Meteor.subscribe('users');
+
     Template.MessageContainer.helpers({
         messages: function() {
             return Messages.find({});
@@ -10,10 +42,10 @@ if (Meteor.isClient) {
     Template.MessageContainer.events({
         'keydown textarea': function(e) {
             if(!(e.which === 13 && !e.shiftKey)) return;
-            Messages.insert({
-                text: $('textarea').val(),
-                timestamp: new Date(),
-                user: 'someuser'                
+            Meteor.call('addMessage', $('textarea').val(), 'something', function(err) {
+                if(!err) {
+                    $('textarea').val('');
+                }
             });
         }
     });
@@ -24,14 +56,44 @@ if (Meteor.isClient) {
         }
     });
 
+    Template.User.helpers({
+        name: function() {
+            return this.firstname + this.lastname;
+        }
+    });
 
+    Template.UserContainer.helpers({
+        users: function() {
+            return Users.find({});
+        }
+    });
 }
 
 if (Meteor.isServer) {
+    Meteor.publish('messages', function() {
+        return Messages.find({});
+    });
+
+
+    Meteor.publish('users', function() {
+        return Users.find({});
+    });
+
     Meteor.startup(function() {
-        if(Messages.find({}).count({}) === 0) {
+        if(Messages.find({}).count() === 0) {
             Messages.insert({ text: 'This is a message', timestamp: new Date(), user: 'kjohann'});
             Messages.insert({ text: 'This is a another message', timestamp: new Date(), user: 'someone'});
         }    
     });
 }
+
+
+Meteor.methods({
+    addMessage: function(text, user) {
+        Messages.insert({
+            text: text,
+            timestamp: new Date(),
+            user: user                
+        });        
+    }
+});
