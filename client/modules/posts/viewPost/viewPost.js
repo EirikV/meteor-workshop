6ac6b1cb52345482e3b647d9395624c8fdf72d35
EirikV <vigeland.eirik@gmail.com>
2015-11-01 20:39:58 +0100

    Fixed cropping of images and added new random names

--- client/modules/posts/viewPost/viewPost.js
+++ client/modules/posts/viewPost/viewPost.js
@@ -2,41 +2,46 @@ var post;
 var activeText = new ReactiveVar('');
 
 Template.viewPost.helpers({
-	getUser: function(userId) {
-		var user = Meteor.users.findOne(userId);
-		if(!user) return;
-		return {
-			username: user.username,
-			profile: user.profile
-		};
-	},
-	fetchText: function() {
-		var activeTextValue = activeText.get();
-		return activeTextValue && this._id === post._id ? activeTextValue : this.text;
-	}
+    getUser: function(userId) {
+        var user = Meteor.users.findOne(userId);
+        if(!user) return;
+        return {
+            username: user.username,
+            profile: user.profile
+        };
+    },
+    fetchText: function() {
+        var activeTextValue = activeText.get();
+
+        if(!post) {
+            return this.text;
+        }
+
+        return activeTextValue && this._id === post._id ? activeTextValue : this.text;
+    }
 
 });
 
 Template.viewPost.events({
-	'mouseenter .post': function(e) {
-		if(!this.text){
-			return;
-		}
-		post = this;
-	},
+    'mouseenter .post': function(e) {
+        if(!this.text){
+            return;
+        }
+        post = this;
+    },
 
-	'mouseleave .post': function(e) {
-		post = undefined;
-	},
+    'mouseleave .post': function(e) {
+        post = undefined;
+    },
 
-	'mouseenter .tag a': function(e) {
-		if(!post) { 
-			return;
-		}
-		activeText.set(post.text.replace(new RegExp('#' + this.tag, "g"), '<span class="active-tag">#' + this.tag + '</span>'));
-	},
+    'mouseenter .tag a': function(e) {
+        if(!post) {
+            return;
+        }
+        activeText.set(post.text.replace(new RegExp('#' + this.tag, "g"), '<span class="active-tag">#' + this.tag + '</span>'));
+    },
 
-	'mouseleave .tag a': function(e) {
-		activeText.set(post.text);
-	}
+    'mouseleave .tag a': function(e) {
+        activeText.set(post.text);
+    }
 });
